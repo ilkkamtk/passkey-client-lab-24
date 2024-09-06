9965701ca309d79b2f16d23435266e90695855f3
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-09-06 09:28:15 +0300

    update

---
+++ src/hooks/apiHooks.ts
@@ -0,0 +1,73 @@
+import fetchData from '@/lib/fetchData';
+import { UserWithNoPassword } from '@sharedTypes/DBTypes';
+import { LoginResponse, UserResponse } from '@sharedTypes/MessageTypes';
+import { PublicKeyCredentialCreationOptionsJSON } from '@simplewebauthn/types';
+// TODO: add imports for WebAuthn functions
+
+const useUser = () => {
+  // TODO: implement network functions for auth server user endpoints
+  const getUserByToken = async (token: string) => {
+    const options = {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    };
+    return await fetchData<UserResponse>(
+      import.meta.env.VITE_AUTH_API + '/users/token/',
+      options,
+    );
+  };
+
+  const getUsernameAvailable = async (username: string) => {
+    return await fetchData<{ available: boolean }>(
+      import.meta.env.VITE_AUTH_API + '/users/username/' + username,
+    );
+  };
+
+  const getEmailAvailable = async (email: string) => {
+    return await fetchData<{ available: boolean }>(
+      import.meta.env.VITE_AUTH_API + '/users/email/' + email,
+    );
+  };
+
+  return { getUserByToken, getUsernameAvailable, getEmailAvailable };
+};
+
+// Define usePasskey hook
+const usePasskey = () => {
+  // Define postUser function
+  const postUser = async (user: UserWithNoPassword) => {
+    // Set up request options
+    const options: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    };
+
+    // fetch and return qrCodeUrl from 2FA server /auth/setup
+    const registrationResponse = await fetchData<{
+      email: string;
+      options: PublicKeyCredentialCreationOptionsJSON;
+    }>(import.meta.env.VITE_PASSKEY_API + '/auth/setup', options);
+
+    console.log(registrationResponse);
+    // TODO: Fetch setup response
+    // TODO: Start registration process
+    // TODO: Prepare data for verification
+    // TODO: Fetch and return verification response
+  };
+
+  // TODO: Define postLogin function
+  const postLogin = async (email) => {
+    // TODO: Fetch login setup options
+    // TODO: Start authentication process
+    // TODO: Fetch and return login verification response
+  };
+
+  // TODO: Return postUser and postLogin functions
+  return { postUser, postLogin };
+};
+
+export { useUser, usePasskey };
